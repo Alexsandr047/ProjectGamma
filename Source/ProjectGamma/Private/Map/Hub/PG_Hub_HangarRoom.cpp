27fad313f804c9870479e4589a95fdfeae6c2225
alorsung047 <alorsung047@gmail.com>
2023-10-11 10:06:49 +0300

    Change

---
+++ Source/ProjectGamma/Private/Map/Hub/PG_Hub_HangarRoom.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Map/Hub/PG_Hub_HangarRoom.h"
+
+// Sets default values
+APG_Hub_HangarRoom::APG_Hub_HangarRoom()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APG_Hub_HangarRoom::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APG_Hub_HangarRoom::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
